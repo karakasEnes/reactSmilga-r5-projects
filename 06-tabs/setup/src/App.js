8483d971a43e64216c278d0682654b1971693614
Enes Karakaş <enes.karakas.e@gamil.com>
2021-12-06 11:26:09 +0300

    do project 6 tabs

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,77 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+  const [data, setData] = useState([]);
+  const [comps, setComps] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+
+    const c = data.reduce((total, cur) => {
+      total.push(cur.company);
+      return total;
+    }, []);
+
+    setComps(c);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const activeTab = data && data[activeIndex];
+
+  if (!activeTab) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <p className='underline'></p>
+      </div>
+
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {comps.map((company, index) => {
+            const isActive = index === activeIndex ? 'active-btn' : 'false';
+            return (
+              <div
+                key={index}
+                onClick={() => setActiveIndex(index)}
+                className={`job-btn ${isActive}`}
+              >
+                {company}
+              </div>
+            );
+          })}
+        </div>
+
+        <article className='job-info'>
+          <h3>{activeTab.title}</h3>
+          <h4>{activeTab.company}</h4>
+          <p className='job-date'>{activeTab.dates}</p>
+          {activeTab.duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+
+      <button className='btn'>more info</button>
+    </section>
+  );
 }
 
-export default App
+export default App;
