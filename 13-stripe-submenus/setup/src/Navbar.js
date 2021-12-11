194b50d9d0915e7555dd285c03add4dc293c0985
Enes Karakaş <enes.karakas.e@gamil.com>
2021-12-11 11:01:17 +0300

    do project 13, submenu sidebar navbar context relationship, imp

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,52 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+  const displaySubmenu = (e) => {
+    console.log(e.target.textContent);
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    console.log(center, bottom);
+    openSubmenu(page, { center, bottom });
+  };
+
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='' />
+          <button className='btn toggle-btn' onClick={displaySubmenu}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' onMouseMove={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseMove={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseMove={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className='btn signin-btn' onMouseMove={displaySubmenu}>
+          Sign in
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
